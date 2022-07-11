d1f1f051b78a847dee7aafaae6ae996740ffc5fa
Iason Chatzopoulos <jasonjente@gmail.com>
2022-07-11 20:45:09 +0300

    minor fix

--- application/New folder/src/main/java/project/persistence/dao/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package project.persistence.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import project.persistence.model.Customer;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
--- application/New folder/src/main/java/project/persistence/dao/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package project.persistence.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import project.persistence.model.OrderItem;
-
-@Repository
-public interface OrderItemRepository extends CrudRepository<OrderItem, Long> {
-}
